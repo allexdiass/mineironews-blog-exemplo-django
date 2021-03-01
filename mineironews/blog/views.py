8953fccecee78f1587c20486d60a87b66d40b034
Alex Dias <51058328+allexdiass@users.noreply.github.com>
2021-03-01 00:06:04 -0300

    Adicionado css e pagina de detalhes

--- mineironews/blog/views.py
+++ mineironews/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.utils import timezone
 
@@ -7,7 +7,11 @@ from django.utils import timezone
 def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # posts = Post.objects.all().order_by('-published_date') # ordenar acendente
-    posts = Post.objects.all().order_by('published_date') # ordenar decrescente
+    posts = Post.objects.all().order_by('-published_date')  # ordenar decrescente
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+def post_detail(request, pk):
+    # post = Post.objects.get(pk=chave) nao funciona porque nao sabemos se sempre sera passado um ip valido
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
